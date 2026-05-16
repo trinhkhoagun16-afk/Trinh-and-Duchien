4655659cb082de2ac0fac2dbb0cee2603d25a54e
trinhkhoagun16-afk <trinhkhoagun16@gmail.com>
2026-05-16 15:50:01 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Trinh-and-Duchien/',
+
   server: {
     host: '0.0.0.0',
     port: 5173
   },
+
   build: {
     sourcemap: true
   }
